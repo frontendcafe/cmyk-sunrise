414d449cafdc03066eb71a7f34078b38e51676e1
Freud Alexandro Munera Gonzalez <alexandromunera@gmail.com>
2021-04-21 16:40:10 -0500

    Save sales in firestore (#24)
    * Changed card product width
    
    * Add produts to confirm sale
    
    * Register the sale in firestore
    
    Co-authored-by: Freud Múnera <freud.munera@qcode.co>

--- js/_products.js
+++ js/_products.js
@@ -1,30 +1,80 @@
 const db = firebase.firestore();
+let totalSaleSum = 0;
+let cantProductsSaleSum = 0;
 
-function getItemsToSale() {
+function removeCurrentData(tableBody) {
+  while (tableBody.hasChildNodes()) {
+    tableBody.removeChild(tableBody.lastChild);
+  }
+}
+
+function populateTableWithNewData(tableBody) {
   const productsCard = document.querySelectorAll('.products__card');
-  const itemsToSale = [];
+  const templateRow = document.getElementById('confirm-sale-template-row');
+  const totalSale = document.querySelector('.confirm-sale__total-sale');
 
   productsCard.forEach((product) => {
     const name = product.querySelector('.products__title').textContent;
     const price = Number(product.querySelector('.products__price-value').textContent);
     const amount = Number(product.querySelector('.products__amount').textContent);
 
-    if (amount > 0) itemsToSale.push({ name: name, price: price, amount: amount });
+    if (amount > 0) {
+      const rowClone = templateRow.content.cloneNode(true);
+
+      const nameTemplate = rowClone.querySelector('.confirm-sale__name');
+      const amountTemplate = rowClone.querySelector('.confirm-sale__amount');
+      const totalTemplate = rowClone.querySelector('.confirm-sale__total');
+
+      nameTemplate.textContent = name;
+      amountTemplate.textContent = amount;
+      totalTemplate.textContent = `$ ${price * amount}`;
+
+      totalSaleSum += price * amount;
+      cantProductsSaleSum += amount;
+
+      tableBody.appendChild(rowClone);
+    }
   });
 
-  return itemsToSale;
+  totalSale.textContent = `$ ${totalSaleSum}`;
+}
+
+function saveSale() {
+
+  db.collection('sales')
+    .add({
+      amount: totalSaleSum,
+      quantity: cantProductsSaleSum,
+      time: Date().toString(),
+    })
+    .then(() => {
+      alert('Venta registrada !!')
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
 }
 
 export function onLoadProducts() {
   const registerSaleButton = document.getElementById('register-sale');
-  registerSaleButton.addEventListener('click', () => {    
+  const sectionConfirmSale = document.getElementById('section-confirm-sale');
+  const tableBody = document.querySelector('.confirm-sale__table-body');
 
-    const itemsToSell = getItemsToSale();
-    console.log('itemsToSell :>> ', itemsToSell);
+  const registerSaleCancelButton = document.querySelector('.confirm-sale__buttons-cancel');
+  const registerSaleConfirmButton = document.querySelector('.confirm-sale__buttons-confirm');
 
-    const sectionConfirmSale = document.getElementById('section-confirm-sale');
-    sectionConfirmSale.classList.add("confirm-sale-active")
+  registerSaleButton.addEventListener('click', () => {
+    sectionConfirmSale.classList.add('confirm-sale-active');
+
+    removeCurrentData(tableBody);
+    populateTableWithNewData(tableBody);
   });
+
+  registerSaleCancelButton.addEventListener('click', () => {
+    sectionConfirmSale.classList.remove('confirm-sale-active');
+  });
+
+  registerSaleConfirmButton.addEventListener('click', saveSale);
 }
 
 export function getProducts() {
@@ -61,5 +111,5 @@ export function getProducts() {
 
         section.appendChild(productClone);
       });
-    }); 
+    });
 }
