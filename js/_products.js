51e13879b805d6ee1efded3231a2f316d2455398
Freud Múnera <freud.munera@qcode.co>
2021-05-01 11:31:04 -0500

    Reset total when register sale

--- js/_products.js
+++ js/_products.js
@@ -10,10 +10,16 @@ function removeCurrentData(tableBody) {
   }
 }
 
+function showRegisterSaleButton() {
+  const registerSaleButton = document.getElementById('register-sale');
+  registerSaleButton.classList.add('register-sale-button--active');
+}
+
 function populateTableWithNewData(tableBody) {
   const productsCard = document.querySelectorAll('.products__card');
   const templateRow = document.getElementById('confirm-sale-template-row');
   const totalSale = document.querySelector('.confirm-sale__total-sale');
+  totalSaleSum = 0;
 
   productsCard.forEach((product) => {
     const name = product.querySelector('.products__title').textContent;
@@ -59,6 +65,7 @@ function saveSale() {
 
 export function onLoadProducts() {
   const registerSaleButton = document.getElementById('register-sale');
+
   const sectionConfirmSale = document.getElementById('section-confirm-sale');
   const tableBody = document.querySelector('.confirm-sale__table-body');
 
@@ -71,7 +78,9 @@ export function onLoadProducts() {
     sectionConfirmSale.classList.add('confirm-sale-active');
 
     removeCurrentData(tableBody);
+    // resetTotalValue();
     populateTableWithNewData(tableBody);
+
   });
 
   registerSaleCancelButton.addEventListener('click', () => {
@@ -113,6 +122,7 @@ export function getProducts() {
         increaseButton.addEventListener('click', () => {
           amount.textContent = Number(amount.textContent) + 1;
           renderTotals('products');
+          showRegisterSaleButton();
         });
 
         section.appendChild(productClone);
