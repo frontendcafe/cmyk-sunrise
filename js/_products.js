2b0999cb516b1055a0625531849d0950ef942513
Freud Munera <freud.munera@qcode.co>
2021-05-02 22:37:42 -0500

    Block modal sale for interact with background items

--- js/_products.js
+++ js/_products.js
@@ -63,7 +63,6 @@ function checkIfShowModalSale() {
   let cantProducts = 0;
   const productsCard = document.querySelectorAll('.products__card');
   const registerSaleButton = document.getElementById('register-sale');
-  
 
   productsCard.forEach((product) => {
     const amount = Number(product.querySelector('.products__amount').textContent);
@@ -103,6 +102,32 @@ export function onLoadProducts() {
   });
 
   registerSaleConfirmButton.addEventListener('click', saveSale);
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById('register-sale');
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('confirm-sale__buttons-cancel')[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = 'block';
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
 }
 
 export function getProducts() {
@@ -131,7 +156,7 @@ export function getProducts() {
         decreaseButton.addEventListener('click', () => {
           amount.textContent =
             Number(amount.textContent) === 0 ? 0 : Number(amount.textContent) - 1;
-            checkIfShowModalSale();
+          checkIfShowModalSale();
           renderTotals('products');
         });
 
